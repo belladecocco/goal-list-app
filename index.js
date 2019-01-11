db698adc6e191d80d8b03e1d7e7dc78baf2dcc4a
Isabella DeCocco <decoccoisabella@gmail.com>
2019-01-10 20:57:03 -0500

    mongo

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
-const express = require("express");
+(async () => {
+    const express = require("express");
+    const mongo = require("mongodb");
 
-const app = express();
-const learnObj = [];
+    const app = express();
+    const MongoClient = mongo.MongoClient;
+    const url = "mongodb://localhost:27017/";
+    const client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db("goalSetter");
+    const allGoals = db.collection("AllGoals");
 
-app.use(express.json());
+    app.use(express.json());
 
-app.get("/learnObj", (req, res) => {
-    res.send(learnObj);
-});
+    app.get("/goals", async (req, res) => {
+        res.send(await allGoals.find({}).toArray());
+    });
 
-app.post("/learnObj", (req, res) => {
-    if(!req.body.name || !req.body.description || !req.body.resource) {
-        return res.status(404).send("An objective must have a name, description and resource.");
-    };
-    const objective = {
-        name: req.body.name, 
-        description: req.body.description,
-        resource: req.body.resource
-    };
-    learnObj.push(objective);
-    res.send(objective);
-});
+    app.post("/goals", async (req, res) => {
+        if (!req.body.name || !req.body.why || !req.body.resource) {
+            return res.status(404).send("An objective must have a name, why and resource.");
+        };
+        await allGoals.insertOne({
+            name: req.body.name,
+            why: req.body.why,
+            resource: req.body.resource
+        });
+        res.send(await allGoals.find({}).toArray())
+    });
 
-app.get("/learnObj/:objName", (req, res) => {
-    let foundObj = learnObj.find(obj => obj.name === req.params.objName);
-    if(!learnObj.includes(foundObj)) return res.status(404).send("Objective not found. Please try a different name.");
-    res.send(foundObj);
-});
+    app.get("/goals/:objName", async (req, res) => {
+        try {
+            res.send(await allGoals.findOne({ name: req.params.objName }));
+        }
+        catch (e) {
+            res.status(404).send("Objective not found. Please try a different name.");
+        }
+    });
 
-app.put("/learnObj/:objName", (req, res) => {
-    let foundObj = learnObj.find(obj => obj.name === req.params.objName);
-    if(!learnObj.includes(foundObj)) return res.status(404).send("Objective not found. Please try a different name.");
-    foundObj.name = req.body.name;
-    foundObj.description = req.body.description; 
-    foundObj.resource = req.body.resource;
-    res.send(foundObj);
-});
+    app.put("/goals/:objId", async (req, res) => {
+        try {
+            let foundObj = await allGoals.findOneAndUpdate({ _id: mongo.ObjectId(req.params.objId) }, {
+                $set: {
+                    name: req.body.name,
+                    why: req.body.why,
+                    resource: req.body.resource
+                }
+            })
+            res.send(foundObj);
+        } catch (e) {
+            res.status(404).send("Objective not found. Please try a different ID.");
+        }
+    });
 
-app.delete("/learnObj/:objName", (req, res) => {
-    let foundIdx = learnObj.findIndex(obj => obj.name === req.params.objName);
-    if(foundIdx == undefined) return res.status(404).send("Objective not found. Please try a different name.");
-    learnObj.splice(foundIdx, 1);
-    res.send(learnObj);
-});
+    app.delete("/goals/:objId", async (req, res) => {
+        let deleted = await allGoals.deleteOne({ _id: mongo.ObjectId(req.params.objId) });
+        if (deleted == undefined) return res.status(404).send("Objective not found. Please try a different name.");
+        else res.send(deleted);
+    });
 
-app.listen(3000, console.log("Listening on Port 3000"));
+    app.listen(3000, console.log("Listening on Port 3000"));
+})();
